f40882513316304ec902d9ebf66db2695fdaa736
Lucas Lois <lucas.lois@privy.io>
2025-08-05 15:27:26 -0300

    chore: updates to @privy-io/expo@0.58.1

--- metro.config.js
+++ metro.config.js
@@ -5,22 +5,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 const config = getDefaultConfig(__dirname);
 
 const resolveRequestWithPackageExports = (context, moduleName, platform) => {
-  // Package exports in `isows` are incorrect, so we need to disable them
-  if (moduleName === "isows") {
-    const ctx = {
-      ...context,
-      unstable_enablePackageExports: false,
-    };
-    return ctx.resolveRequest(ctx, moduleName, platform);
-  }
-  // Package exports in `zustand@4` are incorrect, so we need to disable them
-  if (moduleName.startsWith("zustand")) {
-    const ctx = {
-      ...context,
-      unstable_enablePackageExports: false,
-    };
-    return ctx.resolveRequest(ctx, moduleName, platform);
-  }
   // Package exports in `jose` are incorrect, so we need to force the browser version
   if (moduleName === "jose") {
     const ctx = {
